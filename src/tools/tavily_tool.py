1f92778f7a480fe43fd80bccadaa1e37b2c53d81
teja <tejapolamarasetty2004@gmail.com>
2026-03-13 22:35:09 +0530

    updated tavily API calls

--- src/tools/tavily_tool.py
+++ src/tools/tavily_tool.py
@@ -1,8 +1,9 @@
-from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_tavily import TavilySearch
 from ..settings import settings
 
+
 def tavily_tool_run(query: str) -> str:
-    tavily_run = TavilySearchResults(max_results=2, tavily_api_key=settings.tavily_api_key)
+    tavily_run = TavilySearch(max_results=2, tavily_api_key=settings.tavily_api_key)
     return tavily_run.invoke(query)
 
 if __name__ == "__main__":
